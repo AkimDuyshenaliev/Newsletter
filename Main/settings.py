576ad3f2128005cdc19968c4544957e1950cd523
AkimC <akim.duyshenaliev@gmail.com>
2019-12-21 20:37:15 +0600

    fixed debug

--- Main/settings.py
+++ Main/settings.py
@@ -26,8 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
-# DEBUG = False
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', config('REDIS_URL'), config('DATABASE_URL')]
 
